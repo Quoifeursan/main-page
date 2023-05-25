998fb3533031e61edfcb79e11cc434c5983d4c80
Quoifeursan <134583797+Quoifeursan@users.noreply.github.com>
2023-05-25 09:57:29 +0200

    Delete compmte rendu html directory

--- compmte rendu html/script.js
+++
@@ -1,6 +0,0 @@
-const menuToggle = document.querySelector('.menu-toggle');
-const sidebar = document.querySelector('.sidebar');
-
-menuToggle.addEventListener('click', () => {
-  sidebar.style.width = sidebar.style.width === '200px' ? '0' : '200px';
-});
